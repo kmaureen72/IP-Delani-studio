ecbc987966d3625328d0a3961a76fca81dd69b01
maureen kagochi <kagochimaureen99@gmail.com>
2022-03-11 21:08:39 -0500

    script for contact section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ $(document).ready(function () {
       $(this).children(".overlay").fadeToggle("slow");
     });
   });
+  //script for contact section
+  $(document).ready(function(){
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    alert("Dear " + name + "  we have received your message. Thank you for reaching out to us. we will get back to you ASAP");
+    document.getElementById("form").reset();
+});
+  });
